5b255f1ff774065d829eb883b8e76ada66ea0544
matheus <matheusgolminif@gmail.com>
2022-03-15 23:11:35 -0300

    add UniqueEntityID

---
+++ src/domain/shared/core/unique-entity-id.ts
@@ -0,0 +1,14 @@
+import { v4 as uuid } from 'uuid';
+import Identifier from './identifier';
+
+/**
+ * @param id is optional as string.
+ * If id is provided returns itself else generate a new uuid
+ */
+export default class UniqueEntityID extends Identifier<string | number> {
+  constructor(id?: string | number) {
+    super(id ? id : uuid());
+  }
+}
+
+export { UniqueEntityID };
