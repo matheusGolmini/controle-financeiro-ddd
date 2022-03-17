dfbf0a287ccf89cdcf62881eadbaf146ba570756
matheus <matheusgolminif@gmail.com>
2022-03-16 22:38:52 -0300

    :heavy_check_mark: add tests for PasswordValueObject

---
+++ src/domain/user/values-objects/password/password.value-object.spec.ts
@@ -0,0 +1,30 @@
+import { PasswordValueObject } from './password.value-object';
+
+describe('password.value-object.', () => {
+  it('Should create a valid password', () => {
+    const passwordMock = '123abc';
+    const password = PasswordValueObject.create(passwordMock);
+
+    expect(password.isSuccess).toBe(true);
+    expect(password.getResult().props.value).toBe(passwordMock);
+  });
+
+  it('Should fail if password is not on range min 3 char and man 20 char', () => {
+    const shortPasswordMock = '1c';
+    const password1 = PasswordValueObject.create(shortPasswordMock);
+
+    expect(password1.isFailure).toBe(true);
+    expect(password1.error).toBe(
+      'Password must have min 3 char and max 20 char',
+    );
+
+    const longPasswordMock = '1c54564654654651526854115';
+    const password2 = PasswordValueObject.create(longPasswordMock);
+
+    expect(password2.isFailure).toBe(true);
+    expect(password2.error).toBe(
+      'Password must have min 3 char and max 20 char',
+    );
+
+  });
+});
