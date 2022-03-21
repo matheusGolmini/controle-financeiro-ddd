13b297f6ada288aeb5e7dc99fd0df2bb08d4875e
matheus <matheusgolminif@gmail.com>
2022-03-21 20:44:25 -0300

    :hammer: changed access modifier

--- src/domain/shared/core/value-object.ts
+++ src/domain/shared/core/value-object.ts
@@ -8,7 +8,7 @@ interface ValueObjectProps {
  */
 
 export default abstract class ValueObject<T extends ValueObjectProps> {
-  public props: T;
+  protected props: T;
 
   constructor(props: T) {
     const baseProps: any = {
