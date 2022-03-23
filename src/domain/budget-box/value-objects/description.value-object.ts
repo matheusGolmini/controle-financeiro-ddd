0313c7a2828870885b636a4615fc6d938f91db9a
matheus <matheusgolminif@gmail.com>
2022-03-22 21:56:20 -0300

    :sparkles: add DescriptionValueObject

---
+++ src/domain/budget-box/value-objects/description.value-object.ts
@@ -0,0 +1,30 @@
+import { Result, ValueObject } from '../../shared';
+
+export interface DescriptionValueObjectProps {
+  value: string;
+}
+
+export class DescriptionValueObject extends ValueObject<DescriptionValueObjectProps> {
+  private constructor(props: DescriptionValueObjectProps) {
+    super(props);
+  }
+
+  get value(): string {
+    return this.props.value;
+  }
+
+  public static create(description: string): Result<DescriptionValueObject> {
+    const isValidLength =
+      description.trim().length >= 1 && description.trim().length <= 30;
+
+    return isValidLength
+      ? Result.ok<DescriptionValueObject>(
+          new DescriptionValueObject({
+            value: description.toLowerCase(),
+          }),
+        )
+      : Result.fail<DescriptionValueObject>(
+          'Invalid description lenght min 1 char and max 30 char',
+        );
+  }
+}
