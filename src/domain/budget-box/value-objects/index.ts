75471dadd3481223a7b8b8caae8cd38f36c487e9
matheus <matheusgolminif@gmail.com>
2022-03-28 20:53:11 -0300

    :sparkles: add ReasonDescriptionValueObject

--- src/domain/budget-box/value-objects/index.ts
+++ src/domain/budget-box/value-objects/index.ts
@@ -1,2 +1,4 @@
-export * from './budget-percentage/budget-percentage.value-object';
+export * from './reason-description/reason-description.value-object';
 export * from './budget-description/budget-description.value-object';
+export * from './budget-percentage/budget-percentage.value-object';
+export * from './percentage/percentage.value-object';
---
+++ src/domain/budget-box/value-objects/reason-description/reason-description.value-object.ts
@@ -0,0 +1,32 @@
+import { Result, ValueObject } from '../../../shared';
+
+export interface ReasonDescriptionValueObjectProps {
+  value: string;
+}
+
+export class ReasonDescriptionValueObject extends ValueObject<ReasonDescriptionValueObjectProps> {
+  private constructor(props: ReasonDescriptionValueObjectProps) {
+    super(props);
+  }
+
+  get value(): string {
+    return this.props.value;
+  }
+
+  public static create(
+    description: string,
+  ): Result<ReasonDescriptionValueObject> {
+    const isValidLength =
+      description.trim().length >= 1 && description.trim().length <= 20;
+
+    return isValidLength
+      ? Result.ok<ReasonDescriptionValueObject>(
+          new ReasonDescriptionValueObject({
+            value: description.toLowerCase(),
+          }),
+        )
+      : Result.fail<ReasonDescriptionValueObject>(
+          'Invalid description lenght min 1 char and max 20 char',
+        );
+  }
+}
