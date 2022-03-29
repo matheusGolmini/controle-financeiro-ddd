00254f00563d5a36e4602069c30204b7b3e44784
matheus <matheusgolminif@gmail.com>
2022-03-28 21:00:21 -0300

    :sparkles: add ReasonIdValueObject

--- src/domain/budget-box/value-objects/index.ts
+++ src/domain/budget-box/value-objects/index.ts
@@ -2,3 +2,4 @@ export * from './reason-description/reason-description.value-object';
 export * from './budget-description/budget-description.value-object';
 export * from './budget-percentage/budget-percentage.value-object';
 export * from './percentage/percentage.value-object';
+export * from './reason-id/reason-id.value-object';
