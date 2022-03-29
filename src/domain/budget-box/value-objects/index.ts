82b945da978b673cabf62bb590b485dd3b4c21bd
matheus <matheusgolminif@gmail.com>
2022-03-28 22:43:47 -0300

    :hammer: unused code remover

--- src/domain/budget-box/value-objects/index.ts
+++ src/domain/budget-box/value-objects/index.ts
@@ -1,5 +1,4 @@
 export * from './reason-description/reason-description.value-object';
 export * from './budget-description/budget-description.value-object';
-export * from './budget-percentage/budget-percentage.value-object';
 export * from './percentage/percentage.value-object';
 export * from './reason-id/reason-id.value-object';
