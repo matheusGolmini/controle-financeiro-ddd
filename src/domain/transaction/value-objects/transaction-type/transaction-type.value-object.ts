6daaeb4a5e74fbe3ed0a9d47d0ee3ac5ad872c46
matheus <matheusgolminif@gmail.com>
2022-03-30 22:11:21 -0300

    :sparkles: add TransactionTypeValueObject

---
+++ src/domain/transaction/value-objects/transaction-type/transaction-type.value-object.ts
@@ -0,0 +1,36 @@
+import { Result, ValueObject } from '../../../shared';
+
+export enum ValidTransactionTypeEnum {
+  'ENTRADA',
+  'SAIDA',
+}
+
+export type transactionType = keyof typeof ValidTransactionTypeEnum;
+
+export interface TransactionTypeValueObjectProps {
+  value: transactionType;
+}
+
+export class TransactionTypeValueObject extends ValueObject<TransactionTypeValueObjectProps> {
+  private constructor(props: TransactionTypeValueObjectProps) {
+    super(props);
+  }
+
+  get value(): transactionType {
+    return this.props.value;
+  }
+
+  public static create(
+    type: transactionType,
+  ): Result<TransactionTypeValueObject> {
+    const isValidEnumValue = Object.values(ValidTransactionTypeEnum).includes(
+      type.toUpperCase(),
+    );
+
+    return isValidEnumValue
+      ? Result.ok<TransactionTypeValueObject>(
+          new TransactionTypeValueObject({ value: type }),
+        )
+      : Result.fail<TransactionTypeValueObject>('Invalid option');
+  }
+}
