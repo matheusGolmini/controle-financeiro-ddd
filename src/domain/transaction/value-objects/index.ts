8485f800739c7be0ec029830fef51bf6b02b7d61
matheus <matheusgolminif@gmail.com>
2022-04-19 22:42:36 -0300

    :hammer: add new paths in transaction value-object index

--- src/domain/transaction/value-objects/index.ts
+++ src/domain/transaction/value-objects/index.ts
@@ -1,1 +1,6 @@
 export * from './transaction-type/transaction-type.value-object';
+export * from './attachment/attachment.value-object';
+export * from './status/status.value-object';
+export * from './transaction-calculations/transaction-calculations.value-object';
+export * from './transaction-note/transaction-note.value-object';
+export * from './transaction-type/transaction-type.value-object';
