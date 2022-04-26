ffad5e417ad1e64b0494e70a89ccaf7ba9668107
matheus <matheusgolminif@gmail.com>
2022-04-25 23:18:40 -0300

    :heavy_check_mark: add test in user mapper

---
+++ src/infra/user/repo/user.mapper.spec.ts
@@ -0,0 +1,87 @@
+import { BudgetIdValueObject } from '@domain-budget-box/value-objects';
+import { UserAggregate } from '@domain-user/aggregates';
+import {
+  EmailValueObject,
+  PasswordValueObject,
+  TermValueObject,
+  IpValueObject,
+} from '@domain-user/values-objects';
+import { DateValueObject } from '@shared-common/index';
+import UniqueEntityID from '@shared-core/unique-entity-id';
+import { User } from '../entities/user.schema';
+import { UserMapper } from './user.mapper';
+
+describe('user.mapper', () => {
+  let domain: UserAggregate;
+  let persistence: User;
+  const currentDate = new Date();
+
+  beforeAll(() => {
+    domain = UserAggregate.create(
+      {
+        email: EmailValueObject.create('email@valid.com').getResult(),
+        password: PasswordValueObject.create('validPassword').getResult(),
+        totalBalanceAvalable: 0,
+        budgetBoxIds: [
+          BudgetIdValueObject.create(
+            new UniqueEntityID('valid_id'),
+          ).getResult(),
+        ],
+        terms: [
+          TermValueObject.create({
+            acceptedAt: DateValueObject.create(currentDate).getResult(),
+            ip: IpValueObject.create('192.158.1.38').getResult(),
+            userAgent: {
+              name: 'firefox',
+              os: 'LINUX',
+              type: 'browser',
+              version: '80.0.1',
+            },
+          }).getResult(),
+        ],
+        createdAt: currentDate,
+        updatedAt: currentDate,
+      },
+      new UniqueEntityID('valid_id'),
+    ).getResult();
+
+    persistence = {
+      budgetBoxIds: ['valid_id'],
+      createdAt: currentDate,
+      email: 'email@valid.com',
+      id: 'valid_id',
+      password: 'validPassword',
+      terms: [
+        {
+          acceptedAt: currentDate,
+          ip: '192.158.1.38',
+          userAgent: {
+            name: 'firefox',
+            os: 'LINUX',
+            type: 'browser',
+            version: '80.0.1',
+          },
+        },
+      ],
+      totalBalanceAvalable: 0,
+      updatedAt: currentDate,
+    };
+  });
+
+  it('Should be defined', () => {
+    const mapper = new UserMapper();
+    expect(mapper).toBeDefined();
+  });
+
+  it('Should convert object from persistence to domain', () => {
+    const mapper = new UserMapper();
+    const result = mapper.toDomain(persistence);
+    expect(result).toEqual(domain);
+  });
+
+  it('Should convert object from domain to persistence', () => {
+    const mapper = new UserMapper();
+    const result = mapper.toPersistence(domain);
+    expect(result).toEqual(persistence);
+  });
+});
